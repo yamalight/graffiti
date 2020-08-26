533a1e4bee9f0f0e62da1d67116ff07247f1b20c
Tim Ermilov <yamalight@gmail.com>
2020-08-26 16:37:27 +0200

    Initial PoC showing it's possible to do

---
+++ bin/graffiti.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const { start } = require('../lib');
+
+start();
---
+++ schema/note.js
@@ -0,0 +1,6 @@
+// notes
+exports.schema = {
+  name: String,
+  body: String,
+  // group: { type: mongoose.Schema.Types.ObjectId, ref: 'Collection' },
+};
